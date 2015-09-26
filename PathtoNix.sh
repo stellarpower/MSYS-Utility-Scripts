12f7f0f8987c946395dbbf55468bdccd116248a3
stellarpower <stellarpower@googlemail.com>
2015-09-26 16:26:38 +0100

    Create PathtoNix.sh

---
+++ PathtoNix.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#   Copyright Ben Southall (github.com/stellarpower) 2015.
+# Distributed under the Boost Software License, Version 1.0.
+#    (See accompanying file LICENCE.txt or copy at
+#          http://www.boost.org/LICENSE_1_0.txt)
+
+
+
+count=$#
+
+for i in "$@" ; do 
+	response="$(  echo "$1" | sed -r  -e 's/\\/\//g' -e 's/^([^:]+):/\/\1/'  )"
+	[ $count -le 1 ] && echo "$response" || echo -e "$response \c" 
+	count=$(( $count - 1 ))
+done
+
+#echo
+
+
