b88081dfe75c844fdcea5acdb9cba363c613fa19
stellarpower <stellarpower@googlemail.com>
2015-09-26 17:35:22 +0100

    Update PathtoNix.sh
    Hadn't changed "$1" to "$i"

--- PathtoNix.sh
+++ PathtoNix.sh
@@ -10,7 +10,7 @@
 count=$#
 
 for i in "$@" ; do 
-	response="$(  echo "$1" | sed -r  -e 's/\\/\//g' -e 's/^([^:]+):/\/\1/'  )"
+	response="$(  echo "$i" | sed -r  -e 's/\\/\//g' -e 's/^([^:]+):/\/\1/'  )"
 	[ $count -le 1 ] && echo "$response" || echo -e "$response \c" 
 	count=$(( $count - 1 ))
 done
